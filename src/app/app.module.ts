1db526117c6f754fbbb06b4954dd25d8ab193ee6
littleOStar <qqduxingzhe@163.com>
2018-12-16 11:54:25 +0800

    init demo; delete component array; use service subject provide data; add product-detail 2 way get data;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+
+import {AppRoutingModule} from './appRoutingModule';
+import {ProductDetailService} from './services/product-detail.service';
+import {ProductListService} from './services/product-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductDetailService,
+    ProductListService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
